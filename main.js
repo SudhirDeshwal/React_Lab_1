5f5affe6f908c8bd2e012340863b35465cf5735b
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-29 03:04:32 -0400

    Code set up done

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
+//Function for header
 function TaskList_header(props) {
 
       return (
-           <div className = 'Tasks'> 
-            <h1>{props.heading} </h1>
-            <h3> Number of Tasks : {props.items}</h3>
+           <div> 
+            <h4>{props.heading} </h4>
+            <h4>Total Number of Tasks : {props.items}</h4>
            </div>
            
            );
         }
 
+//Function for list blocks
+              function Task_blocks_list(props) {
 
-        
-        function Task_blocks(props) {
-
-            return (
-                 <div className = 'blocks'> 
-                  <h4>{props.block_heading} </h4>
-                  <ul> 
-                         <li>{props.item1}</li>
-                         <li>{props.item2}</li>
-                         <li>{props.item3}</li>
-                  </ul>
-                  
-                 </div>
-                 
-                 );
-              }
-
+                return (
+                     <div> 
+                    <h4>{props.heading} </h4>
+                      <ul> 
+                             <li>{props.item1}</li>
+                             <li>{props.item2}</li>
+                             <li>{props.item3}</li>
+                      </ul>
+                     </div>
+                     
+                     );
+                  }
+//Storing components in one variable
      var app = (
         <div>
-         <TaskList_header heading="Task List"  items="9"></TaskList_header>
-         <Task_blocks block_heading="Humber" item1="Task1" item2="Task2" item3="Task3"></Task_blocks>
-         <Task_blocks block_heading="MERN" item1="Lab" item2="Project" item3="Quiz"></Task_blocks>
-         <Task_blocks block_heading="Java" item1="Group Discussion" item2="Exam" item3="Assignment"></Task_blocks>
+         <TaskList_header heading="Task List" items="9"/>
+         <Task_blocks_list heading="Humber" item1="Task1" item2="Task2" item3="Task3"/>
+         <Task_blocks_list heading="MERN" item1="Lab" item2="Project" item3="Quiz"/>
+         <Task_blocks_list heading="Java"item1="Group Discussion" item2="Exam" item3="Assignment"/>
          </div>
              );
 
+//Appending variable into the DOM
     ReactDOM.render(app , document.getElementById("root"));
 
     
